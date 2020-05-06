7284a00cc18add332844294e6bec85a85194b207
abdulghafurjober <abdulghafurjober@hotmail.com>
2020-05-06 12:09:26 +0800

    front end add 1

--- app/controllers/rider_controller.rb
+++ app/controllers/rider_controller.rb
@@ -1,4 +1,9 @@
 class RiderController < ApplicationController
   def index
   end
+
+  def new
+    #@rider = Rider.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'assignment/index'
-  get 'assignment/index2'
+  get 'assignment/new'
   get 'rider/index'
-  get 'rider/index2'
+  get 'rider/new'
   get 'report/index'
   get 'report/index2'
 end
