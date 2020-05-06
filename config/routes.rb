6c096fb92a8f11e00bf0f5b26aedcc25e0e863a9
abdulghafurjober <abdulghafurjober@hotmail.com>
2020-05-06 13:18:03 +0800

    add show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'assignment/index'
   get 'assignment/new'
+  get 'assignment/show'
   get 'rider/index'
   get 'rider/new'
+  get 'rider/show'
   get 'report/index'
   get 'report/index2'
 end
