c81aa15ee4582018821341eafd92500b2ce0fb3c
abdulghafurjober <abdulghafurjober@hotmail.com>
2020-05-09 11:33:25 +0800

    litthe fix

--- app/controllers/riders_controller.rb
+++ app/controllers/riders_controller.rb
@@ -69,6 +69,6 @@ class RidersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def rider_params
-      params.require(:rider).permit(:name, :IC, :Phone, :Location, :Status, :Picture, :BankAccNo, :BankName)
+      params.require(:rider).permit(:name, :ic, :phone, :location, :status, :picture, :bankacc, :bankname)
     end
 end
